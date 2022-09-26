3511e3604fb7ef9441309f5d10b4e408597d2bef
Vrits <saidhalim687@gmail.com>
2022-09-26 19:44:47 +0800

    Making Sideitem to dynamic components

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,20 +1,24 @@
-import { Box, Flex, useColorModeValue } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 import SideItem from "./Sideitem";
 
 const Sidebar = () => {
-  const bgCol = useColorModeValue("gray.100", "gray.700");
   return (
-    <Flex flexDirection={"column"} w={"400px"} alignItems={"center"}>
-      <Box
-        w={"full"}
-        border={"solid 1px"}
-        borderRadius={"lg"}
-        overflow={"hidden"}
-        borderColor={bgCol}
-      >
-        <SideItem />
-      </Box>
-    </Flex>
+    <VStack spacing={5} w={"400px"}>
+      <SideItem
+        title={"Kategori Artikel"}
+        subTitle={["Kegiatan", "Pengumuman", "Dokumentasi"]}
+      />
+      <SideItem
+        title={"Navigasi Situs"}
+        subTitle={[
+          "Sejarah STMIK Indonesia",
+          "Visi Misi dan Tujuan STMIK Indonesia",
+          "Semua Download",
+          "Semua Artikel",
+          "Hubungi Kami",
+        ]}
+      />
+    </VStack>
   );
 };
 
--- src/components/Sidebar/Sideitem.jsx
+++ src/components/Sidebar/Sideitem.jsx
@@ -8,45 +8,50 @@ import {
   VStack,
 } from "@chakra-ui/react";
 
-const SideItem = () => {
+const SideItem = ({ title, subTitle }) => {
   const bgCol = useColorModeValue("gray.100", "gray.700");
 
   return (
-    <>
-      <Box display={"flex"} p={3} bg={bgCol} w={"full"}>
-        <Center>
-          <InfoIcon />
-          <Text fontSize={"md"} mx={2}>
-            Kategori Artikel
-          </Text>
-        </Center>
-      </Box>
       <Box
-        m={5}
-        p={3}
-        borderRadius={"lg"}
+        w={"full"}
         border={"solid 1px"}
+        borderRadius={"lg"}
+        overflow={"hidden"}
         borderColor={bgCol}
       >
-        <VStack
-          spacing={1}
-          divider={<StackDivider borderColor={bgCol} />}
-          // textAlign={"center"}
+        <Box display={"flex"} p={3} bg={bgCol} w={"full"}>
+          <Center>
+            <InfoIcon />
+            <Text fontSize={"md"} mx={2}>
+              {title}
+            </Text>
+          </Center>
+        </Box>
+        <Box
+          m={5}
+          p={3}
+          borderRadius={"lg"}
+          border={"solid 1px"}
+          borderColor={bgCol}
         >
-          <Text
-            fontSize={"sm"}
-            w={"full"}
-            p={3}
-            borderRadius={"lg"}
-            transition={"ease-out .5s all"}
-            _hover={{
-              cursor: "pointer",
-              bg: useColorModeValue("gray.100", "gray.700"),
-            }}
-          >
-            Kegiatan
-          </Text>
-          <Text
+          <VStack spacing={1} divider={<StackDivider borderColor={bgCol} />}>
+            {subTitle.map((item, index) => (
+              <Text
+                fontSize={"sm"}
+                w={"full"}
+                p={3}
+                borderRadius={"lg"}
+                transition={"ease-out .5s all"}
+                _hover={{
+                  cursor: "pointer",
+                  bg: useColorModeValue("gray.100", "gray.700"),
+                }}
+                key={item + index}
+              >
+                {item}
+              </Text>
+            ))}
+            {/* <Text
             fontSize={"sm"}
             w={"full"}
             p={3}
@@ -71,10 +76,10 @@ const SideItem = () => {
             }}
           >
             Dokumentasi
-          </Text>
-        </VStack>
+          </Text> */}
+          </VStack>
+        </Box>
       </Box>
-    </>
   );
 };
 
